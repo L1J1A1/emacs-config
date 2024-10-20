82eeef41a8159927c61fe58c00992eac29b8e767
Gavin <rosette.ljy@gmail.com>
2024-10-20 16:47:32 +0800

    emacs config

---
+++ init.el
@@ -0,0 +1,22 @@
+
+(add-to-list 'load-path
+	     (expand-file-name (concat user-emacs-directory "lisp")))
+
+
+(require 'init-startup)
+(require 'init-elpa)
+(require 'init-packages)
+(require 'init-ui)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(company drag-stuff restart-emacs use-package smart-mode-line gruvbox-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ lisp/init-startup.el
@@ -0,0 +1,17 @@
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(global-display-line-numbers-mode 1)
+
+(setq display-line-numbers-type 'relative)
+(setq inhibit-startup-screen t)
+(setq make-backup-files nil)
+
+(electric-pair-mode t)
+(add-hook 'prog-mode-hook #'show-paren-mode)
+(add-hook 'prog-mode-hook #'hs-minor-mode)
+
+(add-to-list 'default-frame-alist '(width . 90))
+(add-to-list 'default-frame-alist '(height . 55))
+
+(provide 'init-startup)
