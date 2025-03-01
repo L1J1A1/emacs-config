3a6fc18cb73a7c56ad69ba4dcb23ba4d098918a4
Gavin <rosette.ljy@gmail.com>
2025-03-01 11:45:55 +0800

    some settings

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,7 +1,7 @@
 (setq org-log-done 'time)
 (setq org-agenda-start-on-weekday 0)
 (setq org-ellipsis "⤵")
-
+(setq org-startup-indented t)
 (use-package org-bullets
   :ensure t
   :hook (org-mode . org-bullets-mode))
--- lisp/init-window.el
+++ lisp/init-window.el
@@ -1,4 +1,4 @@
-(global-set-key (kbd "M-1") 'windmove-left)
+(global-set-key (kbd "M-1") 'windmove-right)
 (global-set-key (kbd "M-2") 'windmove-down)
 (setq windmove-wrap-around t)
 
